2bfa255ba95ef0d750c94b1d9a966412b47d2fcb
zyjared <zyjared@outlook.com>
2025-02-26 18:36:37 +0800

    fix: 数据的路由动画

--- src/pages/tables.vue
+++ src/pages/tables.vue
@@ -2,14 +2,15 @@
   <Nav />
   <div
     class="h-[calc(100vh-3.5rem)]"
-    overflow-auto
+    overflow-y-auto
+    overflow-x-hidden
     mx-auto
     relative
     z-1
     container
   >
     <router-view v-slot="{ Component, route }">
-      <transition name="slide">
+      <transition name="slide" mode="out-in">
         <keep-alive>
           <component :is="Component" :key="route.path" />
         </keep-alive>
@@ -19,17 +20,25 @@
 </template>
 
 <style scoped>
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
+.slide-enter-active {
+  transition: all 0.5s;
 }
 
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
+/* .slide-leave-active {
+  transition: all 0.5s;
+} */
 
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
+.slide-enter-from {
+  transform: translateY(10px);
   opacity: 0;
 }
+
+/* .slide-leave-to {
+  transform: translateY(-10px);
+  opacity: 0;
+} */
+
+/* .slide-leave-active {
+  position: absolute;
+} */
 </style>
