04f0bdd3a64965ba007a657802d63ab8a7659da9
blackwang08 <1580716052@qq.com>
2025-08-25 09:22:58 +0800

    feat✨: 新增第四次结果

--- src/components/section/SectionHistory.vue
+++ src/components/section/SectionHistory.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const results = [
+  { text: '第四次6星强度投票结果（截至余和烛煌)', to: 'https://github.com/ArknightsVote/ArknightsVote/blob/main/result_V2.0.jpeg' },
   { text: '第三次6星强度投票结果(截至艾拉)', to: 'https://github.com/SYadda/Arknights_6Star_Rank_Vote/blob/main/result_V1.3.jpeg' },
   { text: '6星厨力投票结果', to: 'https://nga.178.com/read.php?tid=39828466' },
   { text: '第二次6星强度投票结果(截至锏)', to: 'https://nga.178.com/read.php?tid=38662591' },
