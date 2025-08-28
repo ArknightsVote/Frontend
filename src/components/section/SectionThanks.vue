e4e6d1501afd7e7fb6af262b9aecc55630bfe0ba
Shawn Sun <lengyanyu258@outlook.com>
2025-08-28 20:00:06 +0800

    docs(sectionthanks): revise acknowledgments and add new contributors

--- src/components/section/SectionThanks.vue
+++ src/components/section/SectionThanks.vue
@@ -1,7 +1,16 @@
 <template>
   <Section title="鸣谢">
     <p>
-      本投票两两比较的灵感来源于NGA，制作过程中得到了PRTS.wiki，血狼破军和各位群友、网友的支持和建议，在此向各位表示感谢！
+      <span class="text-gray-600">本投票两两比较的思路来源于</span>
+      <AppLink to="https://ngabbs.com/read.php?tid=36563614" class="text-blue-600 hover:text-blue-800 underline">
+        NGA@InfusedRaindrop
+      </AppLink>
+      <span class="text-gray-600">，在数据爬取和信息展示中，得到了</span>
+      <AppLink to="https://prts.wiki/" class="text-blue-600 hover:text-blue-800 underline">
+        PRTS.wiki
+      </AppLink>
+      <span class="text-gray-600">
+        和阿里云高校计划服务器的支持，在程序设计制作过程中，还得到了血狼破军和各位群友、网友的支持和建议，在此向各位表示感谢！</span>
     </p>
   </Section>
 </template>
